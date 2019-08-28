a9bf79ef18c7bfa111ac6b4a474f823c118361ff
Mary Farner <maryfarner@gmail.com>
2019-08-28 02:10:59 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -12,7 +12,7 @@ def run_guessing_game
   answer = get_num
   prompt_user
   guess = gets.chomp
-  guess = guess.to_I
+  guess = guess.to_i
   
   if guess == answer then
     puts "You guessed the correct number!"
