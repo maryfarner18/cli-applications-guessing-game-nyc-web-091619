9d0484e0b37a8384b81d943f60d70afcda3de3eb
Mary Farner <maryfarner@gmail.com>
2019-08-28 02:04:58 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,8 +1,24 @@
-def get_Num
+def get_num
   rand(1..6)
 end
 
+def prompt_user
+  puts "Guess a number between 1 and 6"
+end
+
+
 
 def run_guessing_game
+  answer = get_num
+  prompt_user
+  guess = gets
+  
+  if guess == answer then
+    puts "You guessed the correct number!"
+  elsif guess == "exit" then
+    puts "Goodbye!"
+  else
+    puts "Sorry, the computer guessed #{answer}."
+  end
   
 end
